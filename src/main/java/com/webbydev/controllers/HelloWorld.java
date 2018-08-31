d99e154c115746c0cbdfa5e1a8c5cae598485d52
Mervyn Kapichi <mervinkapichi@gmail.com>
2018-08-31 14:21:41 +0200

    Second commit for the Spring Boot Project

---
+++ src/main/java/com/webbydev/controllers/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.webbydev.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/webbydev/webbydev/WebbyDevApplication.java
@@ -0,0 +1,12 @@
+package com.webbydev.webbydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbyDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbyDevApplication.class, args);
+    }
+}
